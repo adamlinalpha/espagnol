2eadbda57e4af4fc406e7376ffb37dd8b49185e3
adamlinalpha <130927649+adamlinalpha@users.noreply.github.com>
2023-09-02 00:09:48 +0200

    Update Accueil.js

--- public/Accueil.js
+++ public/Accueil.js
@@ -39,11 +39,11 @@ let correction = 'comer'
 let score = []
 
 let hello = function () {
-  fetch('http://localhost:3000/randomTense')
+  fetch('https://entrainement-espagnol.onrender.com/randomTense')
     .then(response => response.json()).then(function (data) {
       temps.innerText = data.tense;
     }).then(function () {
-      fetch('http://localhost:3000/randomPronoun')
+      fetch('https://entrainement-espagnol.onrender.com/randomPronoun')
         .then(response => response.json()).then(function (data) {
           if (temps.innerText == 'Imperatif') {
             let randVf = Math.random();
@@ -59,7 +59,7 @@ let hello = function () {
           }
         })
     }).then(function () {
-      fetch('http://localhost:3000/randomVerb')
+      fetch('https://entrainement-espagnol.onrender.com/randomVerb')
         .then(response => response.json()).then(function (data) {
           infinitif.innerText = data.verb;
           traduction.innerText = data.translation;
@@ -115,11 +115,11 @@ form.addEventListener('submit', function (evt) {
   scoreS.innerText = `${(score.reduce((a, b) => a + b, 0) / score.length).toFixed(2)}%`;
   addEventListener('keypress', function handler(evt) {
 
-    fetch('http://localhost:3000/randomTense')
+    fetch('https://entrainement-espagnol.onrender.com/randomTense')
       .then(response => response.json()).then(function (data) {
         temps.innerText = data.tense;
       }).then(function () {
-        fetch('http://localhost:3000/randomPronoun')
+        fetch('https://entrainement-espagnol.onrender.com/randomPronoun')
           .then(response => response.json()).then(function (data) {
             if (temps.innerText == 'Imperatif') {
               let randVf = Math.random();
@@ -135,7 +135,7 @@ form.addEventListener('submit', function (evt) {
             }
           })
       }).then(function () {
-        fetch('http://localhost:3000/randomVerb')
+        fetch('https://entrainement-espagnol.onrender.com/randomVerb')
           .then(response => response.json()).then(function (data) {
             infinitif.innerText = data.verb;
             traduction.innerText = data.translation;
