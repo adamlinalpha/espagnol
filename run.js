e9d6f30f90f911db2c86009472f74824b4deb3c4
adamlinalpha <130927649+adamlinalpha@users.noreply.github.com>
2023-09-12 13:50:12 +0200

    Update run.js

--- run.js
+++ run.js
@@ -27,7 +27,7 @@ verbs.forEach(
     const firstLetterCap = firstLetter.toUpperCase()
     const remainingLetters = object.infinitive.slice(1)
     let infinitive = `(${firstLetterCap + remainingLetters})`
-    let perc = Math.pow(object.perc, 1 / 3)
+    let perc = Math.pow(object.perc, 1 / 1)
     const firstLetter2 = object.translation.charAt(0)
     const firstLetterCap2 = firstLetter2.toUpperCase()
     const remainingLetters2 = object.translation.slice(1)
